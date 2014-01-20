8aea5f114e61d9f816dc03d1428955d090f3abc0
Thomas Haddad <thomas.af.haddad@gmail.com>
2014-01-20 17:02:38 +0100

    Signed-off-by: Thomas Haddad <thomas.af.haddad@gmail.com>

--- test.js
+++ test.js
@@ -87,7 +87,8 @@ var Task = Backbone.Model.extend({
 		if(typeof(attributes.completed)!=='boolean'){
 			console.log('do not set bad values!!!!');
 		}
-	}
+	},
+	localStorage: new Backbone.LocalStorage('tasks-backbone'),
 });
 
 var task1 = new Task();
@@ -95,4 +96,60 @@ task1.attributes;
 task1.finished();
 task1.set({'completed':true,validate:true});
 task1.finished();
-task1.save();
+task1.save({name:'toto'});
+task1.fetch();
+console.log(task1.attributes);
+
+
+
+
+var TaskCollection= Backbone.Collection.extend({
+	model:Task, // on définit le model à instancier
+	sort:function(){
+		var sorted= this.sortBy(function(model){
+			return model.get('name')
+;		});
+		taskCollection.reset(sorted) ;
+	},
+	completed:function(){
+		return this.filter(function(task){
+			return task.get('completed'); // renvoie les entrées avec true
+		});
+	},
+	remaining:function(){
+		return this.without.apply(this,this.completed()); // renvoie toutes les entrées du tableau sans celles taggées a true
+	}
+});
+var taskCollection=new TaskCollection(); // instanciation de la collection
+console.log(taskCollection);
+taskCollection.add([
+	{id:0, name:'olivier', completed:true}, // création de l'instance de Task
+	{id:1, name:'adrien', completed:false},
+	{id:2, name:'thomas', completed:true}
+]);
+console.log('Collection size : '+ taskCollection.length);
+
+console.log(taskCollection.get(1).attributes);
+/*taskCollection.reset([ // reset de la collection et repush le nouveau tableau
+	{id:0,name:'go to Gabon',completed:false}
+]);
+console.log('Collection size : '+ taskCollection.length);*/
+
+var mycolsorted= taskCollection.sortBy(function(model){
+	return model.get('name').toLowerCase();
+});
+console.log(mycolsorted);
+
+//OUTPUT
+mycolsorted.forEach(function(model){
+	console.log(model.get('name'));
+});
+
+
+var obj={
+	do_sth: function(){
+		console.log('caca');
+	}
+}
+_.extend(obj,Backbone.Events);
+obj.on('salut',obj.do_sth);
